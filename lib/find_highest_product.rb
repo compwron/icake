c3aa59f3ee7f95548f3950cd32b7c90229abeee4
Linda Goldstein <linda.goldstein@getbraintree.com>
2015-01-04 14:08:25 -0800

    highest product of subset of array

---
+++ lib/find_highest_product.rb
@@ -0,0 +1,16 @@
+# Given an array_of_ints, find the highest_product you can get from three of the integers.
+# The input array_of_ints will always have at least three integers.
+# https://www.interviewcake.com/question/highest-product-of-3
+
+a = [1, 2, 3, 4]
+b = 24
+
+def foo input
+	input.combination(3).to_a.map {|slice| slice.inject(:*)}.max
+end
+
+if foo(a) == b
+	puts "works"
+else
+	puts "nope. got #{a} instead of #{b}"
+end
---
+++ spec/find_highest_product_spec.rb
@@ -0,0 +1,5 @@
+describe FindHighestProduct do
+	it "finds" do
+		
+	end
+end
